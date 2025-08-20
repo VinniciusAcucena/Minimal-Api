bebaa1a83e4029cc91d4cce45a854f6ca5134e34
VinniciusAcucena <acucena39@gmail.com>
2025-08-19 21:48:10 -0300

    criação do get para veiculos

--- Program.cs
+++ Program.cs
@@ -53,6 +53,13 @@ app.MapPost("/veiculos", ([FromBody] VeiculoDTO veiculoDTO, IVeiculoServico veic
     return Results.Created($"/veiculo/{veiculo.id}", veiculo);
 });
 
+app.MapGet("/veiculos", ([FromQuery]int? pagina, IVeiculoServico veiculoServico) =>
+{
+    var veiculos = veiculoServico.Todos(pagina);
+
+    return Results.Ok(veiculos);
+});
+
 app.UseSwagger();
 app.UseSwaggerUI();
 
